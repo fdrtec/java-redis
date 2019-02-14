70d32c031414789a2348ac5328225b683c893719
Julian Luís <julian.lfs@gmail.com>
2019-02-14 21:18:32 -0200

    modelagem

--- src/main/java/challenge/NeighborhoodMongo.java
+++ src/main/java/challenge/NeighborhoodMongo.java
@@ -1,10 +1,52 @@
 package challenge;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.geo.GeoJsonMultiPolygon;
+import org.springframework.data.mongodb.core.geo.GeoJsonPolygon;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * Classe para mapear o bairro no MongoDB
  *
  */
+@Document(collection = "neighborhoods")
 public class NeighborhoodMongo {
 
+    @Id
+    private String id;
+    private String name;
+    private GeoJsonPolygon geometry;
 
+    public NeighborhoodMongo() {
+    }
+
+    public NeighborhoodMongo(String id, String name, GeoJsonPolygon geometry) {
+        this.id = id;
+        this.name = name;
+        this.geometry = geometry;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GeoJsonPolygon getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(GeoJsonPolygon geometry) {
+        this.geometry = geometry;
+    }
 }
--- src/main/java/challenge/RestaurantMongo.java
+++ src/main/java/challenge/RestaurantMongo.java
@@ -1,10 +1,52 @@
 package challenge;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * Classe para mapear o restaurante no MongoDB
  *
  */
+
+@Document(collection = "restaurant")
 public class RestaurantMongo {
 
+    @Id
+    private String id;
+    private GeoJsonPoint location;
+    private String name;
 
+    public RestaurantMongo(String id, GeoJsonPoint location, String name) {
+        this.id = id;
+        this.location = location;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public RestaurantMongo() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public GeoJsonPoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(GeoJsonPoint location) {
+        this.location = location;
+    }
 }
